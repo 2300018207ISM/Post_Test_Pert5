0ad06c0140d5d9ba7ee5836b1f3fa5449432ab68
FegterID <fegasbima@gmail.com>
2026-05-13 00:06:40 +0700

    TUGAS POST TEST PERT 5

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,19 @@ Route::get('/', function () {
 Route::get('/hello', function () {
     return ('Hello World!');
 });
+
+Route::get('/Page', function () {
+    return ('Ini adalah halaman pertama saya dengan laravel');
+});
+
+Route::get('/hello', function () {
+    return ('Nama saya Ismail, Nim saya 207');
+});
+
+Route::get('/user/{name?}', function (string $name = 'Ismail') {
+    return 'Halo, ' . $name;
+});
+
+Route::get('/produk/{id?}', function ($id = '207') {
+    return "Anda sedang melihat produk dengan ID: " . $id;
+});
